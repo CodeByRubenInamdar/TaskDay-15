29df741ce1131e2c11411023f4851fdbc68c7db8
CodeByRubenInamdar <rubeninamdar@gmail.com>
2024-10-22 19:39:30 +0530

    Commit

---
+++ TaskDay-15/src/main/java/com/demo/Back_end/TaskDay15Application.java
@@ -0,0 +1,13 @@
+package com.demo.Back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskDay15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskDay15Application.class, args);
+	}
+
+}
---
+++ TaskDay-15/src/test/java/com/demo/Back_end/TaskDay15ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.Back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskDay15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
